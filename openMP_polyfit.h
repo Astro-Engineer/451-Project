591a6f0a9825c68c831df2f05886649350ec6de5
Isaiah Lee <iclee@usc.edu>
2023-12-01 02:53:53 -0800

    Update openMP_polyfit.h

--- openMP_polyfit.h
+++ openMP_polyfit.h
@@ -1,1 +1,28 @@
+#ifndef OPENMP_POLYFIT_H
+#define OPENMP_POLYFIT_H
 
+
+//------------------------------------------------
+// Function Prototypes
+//------------------------------------------------
+
+//--------------------------------------------------------
+// polyfit()
+// Computes polynomial coefficients that best fit a set
+// of input points.
+//
+// Returns 0 if success.
+//--------------------------------------------------------
+int openmp_polyfit( int pointCount, double *xValues, double *yValues, int coefficientCount, double *coefficientResults );
+
+//--------------------------------------------------------
+// polyToString()
+// Produces a string representation of a polynomial from
+// its coefficients.
+// Returns 0 on success.
+//--------------------------------------------------------
+int openmp_polyToString( char *stringBuffer, size_t stringBufferSz, int coeffCount, double *coefficients );
+
+
+
+#endif	// POLYFIT_H
